abfbd8bd012bcf135f41e0c01bcdffa49ae76988
jeon <wjs4222@naver.com>
2023-05-04 00:42:12 +0900

    프로필 완성

--- src/app/components/Sidebar/Profiles.tsx
+++ src/app/components/Sidebar/Profiles.tsx
@@ -1,18 +1,69 @@
 'use client'
 
 import styled from "styled-components"
+import Image from "next/image"
+import Link from "next/link"
+import profilePhoto from '../../../../public/profile.jpg'
+import github from '../../../../public/github.png'
 
 const ProgilesWrap = styled.nav`
-    background:white;
+    background: rgb(57,62,70);
+    background: linear-gradient(180deg, rgba(57,62,70,1) 48%, rgba(234,234,234,1) 100%);
     padding:10px 20px;
     border-radius:10px;
     border:1px solid #eaeaea;
     width:100%;
+    position:relative;
+    overflow:hidden;
+
+    & h4 {
+        text-align:center;
+        margin:10px 0;
+    }
+
+    & * {
+        position:relative;
+        z-index:100;
+    }
+
+    &:before {
+        content:"";
+        position:absolute;
+        left: 0;
+        bottom: 0;
+        height: 170px;
+        width: 100%;
+        background:white;
+        border-top-left-radius:50px;
+        border-top-right-radius:50px;
+        z-index:1;
+    }
+`
+
+const PhotoWrap = styled.div`
+    width:150px;
+    height:150px;
+    border-radius:150px;
+    overflow:hidden;
+    margin:0 auto;
+`
+
+const GithubWrap = styled.div`
+    margin:0 auto;
+    width:25px;
 `
 
 function Progiles() {
     return<ProgilesWrap>
-        내 프로필
+        <PhotoWrap>
+            <Image src={profilePhoto} width={150} height={150} alt="profile"></Image>
+        </PhotoWrap>
+        <h4>JunHui Jeon</h4>
+        <GithubWrap>
+            <Link href="https://github.com/JeonH503">
+                <Image src={github} width={25} height={25} alt="profile-github"></Image>
+            </Link>
+        </GithubWrap>
     </ProgilesWrap>
 }
 
--- src/app/post/NotionRenderer.tsx
+++ src/app/post/NotionRenderer.tsx
@@ -2,6 +2,7 @@
 
 import NotionPost from 'notion-to-react';
 import { FirstBlock } from '@/utils/block-types';
+import styled from 'styled-components';
 
 type Tables = {[key:string]:FirstBlock}
 
@@ -10,8 +11,18 @@ interface Props {
     tables?:Tables
 }
 
+const NotionWrap = styled.div`
+    background:white;
+    border-radius:15px;
+    border:1px solid #eaeaea;
+    padding:40px;
+    margin-bottom:15px;
+`
+
 function NotionRenderer({blocks, tables}:Props) {
-    return <NotionPost blocks={blocks} tables={tables} />
+    return <NotionWrap>
+        <NotionPost blocks={blocks} tables={tables} />
+    </NotionWrap>
 }
 
 export default NotionRenderer;
